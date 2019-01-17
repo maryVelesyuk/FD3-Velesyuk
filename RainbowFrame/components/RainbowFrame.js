91b4677720187bc3f07aed7e65a09da5f2655f34
maryVelesyuk <maryvelesyuk@gmail.com>
2019-01-17 16:37:15 +0300

    Update RainbowFrame.js

--- RainbowFrame/components/RainbowFrame.js
+++ RainbowFrame/components/RainbowFrame.js
@@ -10,10 +10,11 @@ class RainbowFrame extends React.Component {
   render() {
     let childrenJSX = this.props.children;
     let colorsArr = this.props.colors;
-
-    for (let i = 0; i < colorsArr.length; i++) {
+    
+    colorsArr.forEach( (item, i) => {
       childrenJSX = <div style = {{border:"solid 5px " + colorsArr[i], padding:"10px", display: "inline-block"}}> {childrenJSX} </div>
     }
+    );
 
     return (
       childrenJSX
